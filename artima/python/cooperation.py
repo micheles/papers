1cf66bc81bb65d1c9fc9ba3acda20a0b0e458b42
michele.simionato <michele.simionato@localhost>
2008-08-24 08:02:46 +0000

    Added an example of cooperative methods

---
+++ artima/python/cooperation.py
@@ -0,0 +1,20 @@
+"An example of argument passing in cooperative methods"
+
+class A(object):
+    def __init__(self):
+        print 'A'
+
+class B(object):
+    def __init__(self, a=None):
+        print 'B with a=%s' % a
+        super(B, self).__init__(a)
+
+class C(object):
+    def __init__(self, a):
+        print 'C with a=%s' % a
+        super(C, self).__init__()
+
+class D(B, C):
+    def __init__(self):
+        print 'D'
+        super(D, self).__init__()
