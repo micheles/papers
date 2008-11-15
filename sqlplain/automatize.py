0096791997412acf18ac632ca384e7eeb841b1bd
michele.simionato <michele.simionato@localhost>
2008-11-15 06:59:07 +0000

    Changes to automatize.py

--- sqlplain/automatize.py
+++ sqlplain/automatize.py
@@ -1,15 +1,28 @@
-import os
+import os, sys, subprocess
 from sqlplain.configurator import configurator
 from sqlplain.util import createdb
+
+def collect(directory, ext):
+    'Read the files with a given extension from a directory'
+    sql = []
+    for fname in os.listdir(directory):
+        if fname.endswith(ext):
+            #print 'Reading %s ...' % fname
+            sql.append(file(os.path.join(directory, fname)).read())
+    return sql
 
 def makedb(alias):
     db = createdb(configurator.uri[alias], drop=True)
     testdir = configurator.dir[alias]
-    for fname in os.listdir(testdir):
-        if fname.endswith('.sql'):
-            code = file(os.path.join(testdir, fname)).read()
-            db.execute(code)
-
-    
+    schema = collect(testdir, '-schema.sql')
+    data = collect(testdir, '-data.sql')
+    pycode = collect(testdir, '.py')
+    for chunk in schema + data:
+        db.execute(chunk)
+    for chunk in pycode:
+        subprocess.call([sys.executable, '-c', chunk])
+        
 if __name__ == '__main__':
+    makedb('utest')
+    makedb('autest')
     makedb('ftest')
--- sqlplain/tests/books-data.sql
+++ sqlplain/tests/books-data.sql
@@ -1,6 +1,4 @@
-CREATE TABLE score(
-  score VARCHAR(4) PRIMARY KEY);
-
+-- populate score
 INSERT INTO score VALUES ('+');
 INSERT INTO score VALUES ('O');
 INSERT INTO score VALUES ('O+');
@@ -10,11 +8,5 @@ INSERT INTO score VALUES ('OOO');
 INSERT INTO score VALUES ('OOO+');
 INSERT INTO score VALUES ('OOOO');
 
-CREATE TABLE book(
- title VARCHAR(64),
- author VARCHAR(64),
- score VARCHAR(4) REFERENCES score (score),
- PRIMARY KEY (author, title)
-);
-
-INSERT INTO book VALUES ('Expert Python Programming', 'Ziade', 'OOO+');
+--populate books
+INSERT INTO book VALUES ('Expert Python Programming', 'Ziadé', 'OOO+');
