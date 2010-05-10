f0141103fa930bb4bb3187227122df43e87cb466
msimionato <msimionato@msimionato.uxmi.statnet.statpro.com>
2010-05-10 17:27:53 +0200

    Added my talks at PyConIt 2010

---
+++ pypers/pycon10/menu2.py
@@ -0,0 +1,13 @@
+import Tkinter
+root = Tkinter.Tk()
+text = Tkinter.StringVar()
+label = Tkinter.Label(root, textvariable=text)
+menubar = Tkinter.Menu(root)
+menu = Tkinter.Menu(menubar)
+menubar.add_cascade(label='File', menu=menu)
+for item in ['F1','F2', 'F3']:
+    def showmenu(item=item):
+        text.set(text.get() + '\n%s' % item)
+    menu.add_command(label=item, command=showmenu)
+root.config(menu=menubar); label.pack()
+root.mainloop()
---
+++ pypers/pycon10/tco.py
@@ -0,0 +1,4 @@
+def loop(x=-3):
+    1/x
+    return loop(x+1)
+loop()
